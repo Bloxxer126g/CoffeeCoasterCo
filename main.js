29b00daba4f9cecb14a219f8d6c554b7eccec3e2
Bloxxer126g <tristan.raneroyay@proton.me>
2026-05-27 13:21:39 +0100

    Show items

---
+++ main.js
@@ -0,0 +1,31 @@
+async function DisplayItems() {
+    let Items = await fetch("/api/items.json");
+    let Response = await Items.json();
+
+    Response.Items.forEach(element => {
+        let ShownItem = document.createElement("img");
+        ShownItem.src = element.Image;
+        ShownItem.classList = "Product";
+        ShownItem.id = element.Name;
+        let AddedCart = document.createElement("p")
+        AddedCart.classList = "AddedCart"
+        AddedCart.id = `${element.Name}_AddedCart`
+        AddedCart.innerHTML = ""
+        document.getElementById("Items").appendChild(ShownItem);
+        ShownItem.appendChild(AddedCart)
+        console.log(element.Name);
+        console.log(element.Cost);
+        console.log(element.Image);
+
+        ShownItem.addEventListener("click", event => {
+            document.getElementById(`${element.id}_AddedCart`).innerHTML = "Added To Cart!"
+            setTimeout(event => {
+                document.getElementById(`${element.id}_AddedCart`).innerHTML = ""
+            }, 400)
+        })
+    });
+
+    
+}
+
+DisplayItems();
