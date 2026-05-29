06cc44208b040aa1eef9a50eb6559a1803713d5d
Bloxxer126g <tristan.raneroyay@proton.me>
2026-05-29 09:59:26 +0100

    Some QoL with the /Checkout/

--- checkout/main.js
+++ checkout/main.js
@@ -10,13 +10,16 @@ async function CreateCheckout() {
         let data = JSON.parse(rawText);
         
         if (data.url) {
-            console.log("Redirecting to Square:", data.url);
-            window.location.href = data.url;
+            document.getElementById("CheckoutFrame").src = data.url;
         } else {
             console.error("Backend didn't return a URL:", data);
         }
 
     } catch (error) {
+        document.getElementById("LoadingText").innerHTML = error
+        if (error == "Value for `order.line_items` should not be empty.") {
+                    document.getElementById("LoadingText").innerHTML = "Your basket is empty!"
+        }
         console.error("Something went wrong with the fetch:", error);
     }
 }
